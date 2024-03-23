275f215c41df9111c1fa6fd2d65ba8de55be4b63
due-github <164530953+due-github@users.noreply.github.com>
2024-03-24 03:00:33 +0700

    ALB

--- main.tf
+++ main.tf
@@ -52,6 +52,46 @@ resource "aws_instance" "blog" {
 }
 
 
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  
+  security_groups = module.blog_sg.security_group_id
+ 
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      target = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port      = 80
+        }
+
+      }
+    }
+  ]
+ 
+ http_tcp_listenners = [
+  {
+    port                = 80
+    protocol            = "HTTP"
+    target_groups_index = 0
+  }
+ ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
